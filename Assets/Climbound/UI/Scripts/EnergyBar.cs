0d1c5c9bb0b2c4adcc6aaa572f5c45cbd42a2275
Gerard Verdú Castelar <gerard.verdu@estudiant.enti.cat>
2026-01-26 05:15:19 +0100

    Health bar

--- Assets/Climbound/UI/Scripts/EnergyBar.cs
+++ Assets/Climbound/UI/Scripts/EnergyBar.cs
@@ -9,13 +9,30 @@ public class EnergyBar : MonoBehaviour
     public Image energyBarSeg2;
     public Image energyBarSeg3;
 
+    public Image healthBarSeg1;
+    public Image healthBarSeg2;
+    public Image healthBarSeg3;
+    public Image healthBarSeg4;
+    public Image healthBarSeg5;
+
+
     public Sprite energyBarSegFull1;
     public Sprite energyBarSegFull2;
     public Sprite energyBarSegFull3;
 
+    public Sprite healthBarSegFull1;
+    public Sprite healthBarSegFull2;
+    public Sprite healthBarSegFull3;
+
+
     public Sprite energyBarSegEmpty1;
     public Sprite energyBarSegEmpty2;
     public Sprite energyBarSegEmpty3;
+
+    public Sprite healthBarSegEmpty1;
+    public Sprite healthBarSegEmpty2;
+    public Sprite healthBarSegEmpty3;
+
 
     public GameObject player;
 
@@ -44,6 +61,55 @@ public class EnergyBar : MonoBehaviour
                 energyBarSeg1.sprite = energyBarSegEmpty1;
                 energyBarSeg2.sprite = energyBarSegEmpty2;
                 energyBarSeg3.sprite = energyBarSegEmpty3;
+                break;
+            default:
+                break;
+        }
+
+
+        switch (playerScript.playerHealth)
+        {
+            case 100:
+                healthBarSeg1.sprite = healthBarSegFull1;
+                healthBarSeg2.sprite = healthBarSegFull2;
+                healthBarSeg3.sprite = healthBarSegFull2;
+                healthBarSeg4.sprite = healthBarSegFull2;
+                healthBarSeg5.sprite = healthBarSegFull3;
+                break;
+            case 80:
+                healthBarSeg1.sprite = healthBarSegFull1;
+                healthBarSeg2.sprite = healthBarSegFull2;
+                healthBarSeg3.sprite = healthBarSegFull2;
+                healthBarSeg4.sprite = healthBarSegFull2;
+                healthBarSeg5.sprite = healthBarSegEmpty3;
+                break;
+            case 60:
+                healthBarSeg1.sprite = healthBarSegFull1;
+                healthBarSeg2.sprite = healthBarSegFull2;
+                healthBarSeg3.sprite = healthBarSegFull2;
+                healthBarSeg4.sprite = healthBarSegEmpty2;
+                healthBarSeg5.sprite = healthBarSegEmpty3;
+                break;
+            case 40:
+                healthBarSeg1.sprite = healthBarSegFull1;
+                healthBarSeg2.sprite = healthBarSegFull2;
+                healthBarSeg3.sprite = healthBarSegEmpty2;
+                healthBarSeg4.sprite = healthBarSegEmpty2;
+                healthBarSeg5.sprite = healthBarSegEmpty3;
+                break;
+            case 20:
+                healthBarSeg1.sprite = healthBarSegFull1;
+                healthBarSeg2.sprite = healthBarSegEmpty2;
+                healthBarSeg3.sprite = healthBarSegEmpty2;
+                healthBarSeg4.sprite = healthBarSegEmpty2;
+                healthBarSeg5.sprite = healthBarSegEmpty3;
+                break;
+            case <= 0:
+                healthBarSeg1.sprite = healthBarSegEmpty1;
+                healthBarSeg2.sprite = healthBarSegEmpty2;
+                healthBarSeg3.sprite = healthBarSegEmpty2;
+                healthBarSeg4.sprite = healthBarSegEmpty2;
+                healthBarSeg5.sprite = healthBarSegEmpty3;
                 break;
             default:
                 break;
